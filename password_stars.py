ef1166d69ea8073abee455e006aaa6b7cdc23690
hezhaoqing <Qingq3707@gmail.com>
2025-05-31 16:35:54 +0800

    Add Password check program

---
+++ password_stars.py
@@ -0,0 +1,13 @@
+MIN_LENGTH = 8
+
+
+def main():
+    password = input("Enter password: ")
+    while len(password) < MIN_LENGTH:
+        print(f"Password must be at least {MIN_LENGTH} characters long.")
+        password = input("Enter password: ")
+
+    print('*' * len(password))
+
+
+main()
