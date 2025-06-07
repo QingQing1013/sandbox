d4352b6892a3f020d8b7f5c4914d6d0d206b9de3
hezhaoqing <Qingq3707@gmail.com>
2025-06-07 23:36:01 +0800

    Add program to check password

--- password_stars.py
+++ password_stars.py
@@ -1,13 +1,22 @@
+"""
+CP1404/CP5632 - Practical
+password stars
+"""
 MIN_LENGTH = 8
 
 
 def main():
+    password = get_password()
+    print_asterisks(password)
+
+def get_password():
     password = input("Enter password: ")
     while len(password) < MIN_LENGTH:
         print(f"Password must be at least {MIN_LENGTH} characters long.")
         password = input("Enter password: ")
+    return password
 
-    print('*' * len(password))
-
+def print_asterisks(password):
+    print("*" * len(password))
 
 main()
